8abd8218586d71e35dd40d8af07b6c0523444431
haotian-chen-ooh <haotian.chen@oohmedia.com.au>
2024-05-20 16:59:28 +1000

    nextauth

---
+++ components/AuthWidget.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import { useSession, signIn, signOut, SessionProvider } from "next-auth/react";
+
+const SignInButton = () => <button onClick={() => signIn()}>Sign in</button>;
+const SignOutButton = () => <button onClick={() => signOut()}>Sign out</button>;
+
+const AuthWidget = () => {
+  const { data } = useSession();
+
+  return (
+    <div>
+      {data ? <SignOutButton /> : <SignInButton />}
+      <div>session data: {JSON.stringify(data)}</div>
+    </div>
+  );
+};
+
+export const WrappedAuthWidget = () => {
+  return (
+    <SessionProvider>
+      <AuthWidget />
+    </SessionProvider>
+  );
+};
