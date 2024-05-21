a3e81817259212e57129b0e7c1c7191397a76285
haotian-chen-ooh <haotian.chen@oohmedia.com.au>
2024-05-21 16:00:45 +1000

    middleware

---
+++ app/profile/page.tsx
@@ -0,0 +1,7 @@
+export default async function Profile() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div>Profile page</div>
+    </main>
+  );
+}
